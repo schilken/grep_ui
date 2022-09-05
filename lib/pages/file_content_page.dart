28640aea6488034d17820b5abdcfab37bbedcc2f
Alfred Schilken <alfred@schilken.de>
2022-09-05 13:22:15 +0200

    add refresh button to FileContentPage

--- lib/pages/file_content_page.dart
+++ lib/pages/file_content_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:macos_ui/macos_ui.dart';
 import 'package:provider/provider.dart';
@@ -8,10 +9,19 @@ import '../components/get_custom_toolbar.dart';
 import '../services/files_repository.dart';
 import 'package:mixin_logger/mixin_logger.dart' as log;
 
-class FileContentPage extends StatelessWidget {
+class FileContentPage extends StatefulWidget {
   const FileContentPage({super.key, required this.filePath});
 
   final String filePath;
+
+  @override
+  State<FileContentPage> createState() => _FileContentPageState();
+}
+
+class _FileContentPageState extends State<FileContentPage> {
+  void _refresh() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +43,11 @@ class FileContentPage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       SelectableText(
-                        'Content of: $filePath',
+                        'Content of: ${widget.filePath}',
+                      ),
+                      MacosIconButton(
+                        icon: const MacosIcon(CupertinoIcons.refresh),
+                        onPressed: _refresh,
                       ),
                       const Spacer(),
                     ],
@@ -42,7 +56,7 @@ class FileContentPage extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.fromLTRB(12, 20, 20, 20),
                   child: SizedBox(
-                    height: windowHeight - 150,
+                    height: windowHeight - 165,
                     child: LayoutBuilder(builder: (context, constraints) {
                       log.i('LayoutBuilder ${constraints.maxHeight}');
                       return SingleChildScrollView(
@@ -50,7 +64,7 @@ class FileContentPage extends StatelessWidget {
                         child: FutureBuilder<String>(
                             future: context
                                 .read<FilesRepository>()
-                                .readFile(filePath),
+                                .readFile(widget.filePath),
                             builder: (context, snapshot) {
                               if (snapshot.hasData) {
                                 return Text(snapshot.data!,
